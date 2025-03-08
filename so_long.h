3a9054e1d095584e1a6e82e4fe13422f70944ad3
Shiyun <serene.hoshiyun@gmail.com>
2025-03-08 21:34:45 +0800

    Added data struct to store map vector

--- so_long.h
+++ so_long.h
@@ -6,7 +6,7 @@
 /*   By: hshi-yun <hshi-yun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/08 17:30:56 by hshi-yun          #+#    #+#             */
-/*   Updated: 2025/03/08 21:18:56 by hshi-yun         ###   ########.fr       */
+/*   Updated: 2025/03/08 21:31:50 by hshi-yun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,13 @@
 # include "./libft/libft.h"
 # include <fcntl.h>
 # include <stdio.h>
+
+typedef struct	s_map
+{
+	char	**grid;
+	int		x;
+	int		y;
+}				t_map;
 
 // Error Utils
 void	handle_error(char *error_msg);
